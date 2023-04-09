521f71cb72c9e4d7c742e1c514686e12b833c654
Ty Rickers <ty.rickers@gmail.com>
2023-04-09 05:53:16 -0600

    removed console logs and started update function

--- snake.js
+++ snake.js
@@ -2,15 +2,18 @@ export const SNAKE_SPEED = 2;
 const snakeBody = [{ x: 11, y: 11 }];
 
 export function update() {
-  console.log("update snake");
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+
+  snakeBody[0];
 }
 
 export function draw(gameBoard) {
-  console.log("draw snake");
   snakeBody.forEach((segment) => {
     const snakeElement = document.createElement("div");
-    snakeElement.style.gridRowStart = segment.x;
-    snakeElement.style.gridColumnStart = segment.y;
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
     snakeElement.classList.add("snake");
     gameBoard.appendChild(snakeElement);
   });
