94532c3357ae6d1ce6e98db9c6f29468a36040fb
Ty Rickers <ty.rickers@gmail.com>
2023-04-10 06:08:20 -0600

    added gameover scenario

--- game.js
+++ game.js
@@ -2,14 +2,24 @@ import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
+  snakeIntersection,
+  getSnakeHead,
 } from "./snake.js";
 
 import { updateFood, drawFood } from "./food.js";
 
+import { outsideGrid } from "./grid.js";
+
 let lastRenderTime = 0;
 const gameBoard = document.getElementById("game-board");
+let gameOver = false;
 
 function main(currentTime) {
+  checkDeath();
+  if (gameOver) {
+    return alert("You lose");
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -31,4 +41,8 @@ export function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
